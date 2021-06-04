aae6645efba9afee881b6c7772f2b9047554b7b3
Luminatione <patryklazowski@hotmail.com>
2021-06-04 15:57:31 +0200

    Added validators for password and login during registration

---
+++ WebApplication1/Utility/Validators/DefaultValidators/LoginValidator.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+
+namespace WebApplication1.Utility.Release
+{
+	public class LoginValidator : IValidator
+	{
+		private int minLength = 6;
+		private int maxLength = 20;
+		private string charSet =
+			"AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789-_";
+		public bool IsValid(string toValidate)
+		{
+			return toValidate.Length >= minLength && toValidate.Length <= maxLength && toValidate.All(e => charSet.Contains(e));
+		}
+	}
+}
